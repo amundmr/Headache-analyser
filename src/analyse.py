fd6a7a5e8dbb0bda9a5292356099d19d980e765b
amundmr <amund.raniseth@gmail.com>
2021-04-21 08:58:19 +0200

    made headache class

--- src/analyse.py
+++ src/analyse.py
@@ -7,5 +7,32 @@ for line in data:
     string += line
 c = Calendar(string)
 #print(type(c.events))
-events = c.events
-print(events.event())
+set_of_events = c.events
+i=0
+for event in set_of_events:
+    if i<2:
+        print(event)
+    i+=1
+
+class Headache:
+    def __init__(self, 
+                    date,
+                    start,
+                    max_pain,
+                    coffe = False,
+                    wine = False,
+                    sleep_offset = 0,
+                    sleep_depr = 0,
+                    maxalt = False,
+                    maxalt_fix = None,
+                    ):
+        self.date = date
+        self.start = start
+        self.max_pain = max_pain
+        self.coffe = coffe
+        self.wine = wine
+        self.sleep_offset = sleep_offset
+        self.sleep_depr = sleep_depr
+        self.maxalt = maxalt
+        self.maxalt_fix = maxalt_fix
+        print("initiated headache event object")
